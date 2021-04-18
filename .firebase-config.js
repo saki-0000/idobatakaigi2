2fcae97df2dc5f3e606fe2424b3f2f08ac618d40
saki0000 <saki_27_maruko@yahoo.co.jp>
2021-04-18 17:57:03 +0900

    init

---
+++ .firebase-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyC42l2Vr3RTVQyOe2E6DUgkqk9UVQK9CJM',
+  authDomain: 'idobatakaigi-with-ham-4602e.firebaseapp.com',
+  databaseURL:
+    'https://idobatakaigi-with-ham-4602e-default-rtdb.firebaseio.com',
+  projectId: 'idobatakaigi-with-ham-4602e',
+  storageBucket: 'idobatakaigi-with-ham-4602e.appspot.com',
+  messagingSenderId: '499028900324',
+  appId: '1:499028900324:web:48c69be1d68aad02baaff1'
+}
