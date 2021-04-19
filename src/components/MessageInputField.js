8363d00627b54d919d6271973cac3e187647a5ac
saki-0000 <saki_27_maruko@yahoo.co.jp>
2021-04-19 22:59:38 +0900

    gridadd

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+  }
+})
 
 const MessageField = () => {
-  return <div>MessageField</div>
+  const classes = useStyles()
+  return <div className={classes.root}>MessageField</div>
 }
 
 export default MessageField
