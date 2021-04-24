2f5b2b28ba9a8e20adc6d966ab1d9eca94ec9831
saki-0000 <saki_27_maruko@yahoo.co.jp>
2021-04-24 21:49:30 +0900

    gravatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { Avatar, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+import { gravatarPath } from "../gravatar";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px',
   }
 })
 
-const MessageField = () => {
+const MessageField = ({ name }) => {
   const classes = useStyles()
-  return <div className={classes.root}>MessageField</div>
+  const avatarPath = gravatarPath(name)
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar src={avatarPath} />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default MessageField
